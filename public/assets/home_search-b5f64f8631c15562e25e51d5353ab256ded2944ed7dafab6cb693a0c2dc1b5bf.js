ee86c86acfcc10e9cf990d7d8608745b02c4ec80
Franco Mangone <francomangone18@gmail.com>
2015-09-11 15:09:26 -0300

    Precompiled assets and fixed some error with the unobstrusive js.

---
+++ public/assets/home_search-b5f64f8631c15562e25e51d5353ab256ded2944ed7dafab6cb693a0c2dc1b5bf.js
@@ -0,0 +1,32 @@
+/* Check if field is empty and display correct search class */
+
+
+function handleSearchBar(){ 
+	// Disable submit button:
+
+	$("#search-submit").attr('disabled', 'true');
+
+	$("#search").keyup( function(){
+
+		var $self = $(this);
+		var $submit = $("#search-submit");
+
+		if($self.val().length != 0){
+			if(!$submit.hasClass("valid-search")){
+				$submit.addClass("valid-search");
+				$("#search-submit").removeAttr('disabled');
+			}
+		}
+		else if($submit.hasClass("valid-search")){
+			$submit.removeClass("valid-search");
+			$("#search-submit").attr('disabled', 'true');
+		}
+
+	});
+}
+
+$(document).ready( function(){
+	handleSearchBar();
+}).on("page:change", function(){
+	handleSearchBar();
+});
