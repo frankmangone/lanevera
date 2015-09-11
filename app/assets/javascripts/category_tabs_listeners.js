48f96f99a0ac82eecfd57141654cb58f6e01ae85
Franco Mangone <francomangone18@gmail.com>
2015-09-11 13:21:57 -0300

    Cleaned files. Performance issues with products rendering.

---
+++ app/assets/javascripts/category_tabs_listeners.js
@@ -0,0 +1,35 @@
+// For products/index.html.haml
+
+$(document).ready( function(){
+	// The first category tab is the one that just says "Categorías"
+	$tabs = $(".category-tab").not(":first");
+
+	$tabs.each( function(){
+
+		var $tab = $(this);
+		var $delete_tab = $tab.next();
+
+		$tab.find(".btn-symbol").click( function(event){
+			event.preventDefault();
+
+			$tab.slideUp();
+			$tab.next().slideDown();
+		});
+
+		$delete_tab.hide();
+		$delete_tab.find(".btn-symbol").last().click( function(){
+			/* Delete button, do not prevent default behaviour */
+			$delete_tab.slideUp( function(){
+				$(this).remove();
+			});
+		});
+		$delete_tab.find(".btn-symbol").first().click( function(event){
+			/* Show the category again */
+			event.preventDefault();
+			$(this).parent().parent().slideUp();
+			$(this).parent().parent().prev().slideDown();
+		});
+
+	});
+
+});
---
+++ app/assets/javascripts/home_search.js
@@ -0,0 +1,26 @@
+/* Check if field is empty and display correct search class */
+
+$(document).ready( function(){
+
+	// Disable submit button:
+
+	$("#search-submit").attr('disabled', 'true');
+
+	$("#search").keyup( function(){
+
+		var $self = $(this);
+		var $submit = $("#search-submit");
+
+		if($self.val().length != 0){
+			if(!$submit.hasClass("valid-search")){
+				$submit.addClass("valid-search");
+				$("#search-submit").removeAttr('disabled');
+			}
+		}
+		else if($submit.hasClass("valid-search")){
+			$submit.removeClass("valid-search");
+			$("#search-submit").attr('disabled', 'true');
+		}
+
+	});
+});
