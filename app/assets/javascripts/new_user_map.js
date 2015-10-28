b4120ddc05cd47e3c94e630c2f6cd1d9aff06e45
Franco Mangone <francomangone18@gmail.com>
2015-10-28 10:30:27 -0300

    Started fiddling with mapbox

--- app/assets/javascripts/new_user_map.js
+++ app/assets/javascripts/new_user_map.js
@@ -1,6 +1,15 @@
+var map;
+//var click = document.getElementById('click');
+
 function loadMap(){
 	L.mapbox.accessToken = 'pk.eyJ1IjoiZnJhbmttYW5nb25lIiwiYSI6IjFlZjlmZTliYmYwYjljZDJhYmIwNmU1ZDlkOGExMDdkIn0.wSNu880_pH8ZGjMj3CoYWw';
-	var map = L.mapbox.map('map', 'frankmangone.o0mg9e5k').setView([-34.91587454384448, -56.158976554870605], 16);
+	map = L.mapbox.map('map', 'frankmangone.o0mg9e5k').setView([-34.91587454384448, -56.158976554870605], 16);
+
+	map.on('click', function(e){
+		var position =  e.containerPoint.toString() + ', ' + e.latlng.toString();
+		alert(e.latlng.lat);
+		alert(e.latlng.lng);
+	});
 }
 
 $(document).ready( function(){
