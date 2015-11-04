52afc06bd247bba2729cb11908b0944a29253d49
Franco Mangone <francomangone18@gmail.com>
2015-11-04 13:07:37 -0300

    Precompile assets

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :output, "#{path}/log/cron.log"
-# set :environment, "development"
-set :environment, "production"
+set :environment, "development"
+# set :environment, "production"
 
 every 30.minutes do
 	rake "carts:delete_delivered_carts"
