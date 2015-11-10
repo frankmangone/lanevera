8aed3e14a72f03ef33ce515231b69c14a0ec26ef
Franco Mangone <francomangone18@gmail.com>
2015-11-09 22:36:22 -0300

    Finish model validations

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,9 +1,22 @@
 class Offer < ActiveRecord::Base
+
 	belongs_to :product
 
-	validates :price, presence: true
+	validates :price, presence: true, numericality: { greater_than: 0 }
+	validate  :offer_lower_than_price 
 
 	def write_price
 		"$" +	price.to_s
 	end
+
+	private
+
+		def offer_lower_than_price
+			if self.price
+				if self.price > self.product.price
+					self.errors[:price] << "La oferta debe ser menor al precio original"
+				end
+			end
+		end
+
 end
